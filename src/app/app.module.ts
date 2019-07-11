7b1c4ab5d9bfcb79406d8088a117e7212967ffbe
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-11 11:50:23 +0700

    import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
